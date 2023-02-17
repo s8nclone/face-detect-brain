ca7106ff4d6f3909d33de5b5db36a50288d81ea9
s8nclone <97878920+s8nclone@users.noreply.github.com>
2023-02-17 17:52:05 +0100

    adding more components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLink/ImageLinkForm';
+import Rank from './components/rank/Rank';
 import './App.css';
-
+import bgimage from './bgimage.png';
 class App extends Component {
   render () {
+    const myStyle={
+      backgroundImage: `url(${bgimage})`,
+      height:'100vh',
+      marginTop:'0px',
+      fontSize:'50px',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    };
+
     return (
-      <div className="App">
+      <div className="App" style={myStyle}>
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* {
         
--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -5,9 +5,9 @@ import './logo.css';
 
 const Logo = () => (
     <div className="ma4 mt0">
-        <Tilt className="Tilt" options={{ max: 25 }} style={{ height: 200, width: 200 }} >
+        <Tilt className="Tilt" options={{ max: 25 }} style={{ height: 100, width: 100 }} >
             <div className="Tilt-inner pa3">
-                <img style={{paddingTop: '5px'}} src={brain} alt="brain logo" />
+                <img style={{paddingTop: '3px'}} src={brain} alt="brain logo" />
             </div>
         </Tilt>
     </div>
